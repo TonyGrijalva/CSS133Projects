2ca29750b8ceb9709092fda43c7d5597aec14fd3
Marcos Grijalva <shonish04@gmail.com>
2021-01-20 13:17:26 -0700

    Module4Assignment

---
+++ CS_133_Projects/CS_133_Projects/Module4Assignement/Module4Assignment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace CS_133_Projects.Module_3
+{
+    public class Module_3_Assignment
+    {
+        public Module_3_Assignment()
+        {
+            Cake myClass = new Cake();
+            myClass.flavor = "Vanilla";
+
+
+            Cake myClass2 = new Cake();
+            myClass2.flavor = "Chocolate";
+        }
+    }
+}
+
+public class Cake
+{
+    public string Flavor { get; set; }
+
+    public void ThisIsAMethod<T>(T myParameter)
+    {
+        this.Flavor = myParameter.ToString();
+    }
+}
