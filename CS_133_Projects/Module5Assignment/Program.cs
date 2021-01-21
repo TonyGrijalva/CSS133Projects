799efd89a9ea3cd9757a640ab111b643de31a4b6
Marcos Grijalva <shonish04@gmail.com>
2021-01-21 13:11:50 -0700

    Module 5 Assignment

---
+++ CS_133_Projects/Module5Assignment/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CS_133_Projects
+{
+    public class Program
+    {
+        static void Main()
+        {
+            
+            Stack<string> webpage = new Stack<string>();
+
+           
+            webpage.Push("www.SULearning.com");
+
+            foreach (var item in webpage)
+            {
+                Console.WriteLine(item);
+            }
+
+            string previousPage = webpage.Peek();
+
+            Console.WriteLine(previousPage);
+
+            foreach (var item in webpage)
+            {
+                Console.WriteLine(item);
+            }
+
+            string forwardOnePage = webpage.Pop();
+
+            Console.WriteLine(forwardOnePage);
+
+            foreach (var item in webpage)
+            {
+                Console.WriteLine(item);
+            }
+
+          
+        }
+    }
+   
+}
