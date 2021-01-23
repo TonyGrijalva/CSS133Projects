dbf61eee98447665262a3492ce6597e6f0f81c45
Marcos Grijalva <shonish04@gmail.com>
2021-01-23 16:01:27 -0700

    Module 6 Assignment

---
+++ CS_133_Projects/CS_133_Projects/Module6Assignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Module6
+{
+    class Program
+    {
+        static void Main()
+        {
+ 
+            Hospital myHospital = new Hospital();
+
+            myHospital.AddPatientsIntoTheQueue("First Patient");
+            myHospital.AddPatientsIntoTheQueue("Second Patient");
+            myHospital.AddPatientsIntoTheQueue("Third Patient");
+            myHospital.AddPatientsIntoTheQueue("Fourth Patient");
+            myHospital.AddPatientsIntoTheQueue("Fifth Patient");
+
+        }
+    }
+
+    public class Hospital
+    {
+        private Queue<string> waitingQueue = new Queue<string>();
+
+        public void AddPatientsIntoTheQueue(string parameter)
+        {
+            waitingQueue.Enqueue(parameter);
+
+        }
+
+    }
+}
