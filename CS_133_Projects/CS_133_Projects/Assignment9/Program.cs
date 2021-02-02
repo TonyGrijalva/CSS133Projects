5b85e4474552541ef8f42f0a4952a5e04fc3de4c
Marcos Grijalva <shonish04@gmail.com>
2021-02-01 19:21:20 -0700

    Assignment 9

---
+++ CS_133_Projects/CS_133_Projects/Assignment9/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Assignment9
+{
+    class Program
+    {
+
+        public delegate void MyDelegarePointer(int a, int b);
+
+
+        static void Main()
+        {
+            MyDelegarePointer pointer;
+
+            MyClass newClass = new MyClass();
+            pointer = newClass.MyFirstMethod;
+            pointer += newClass.MySecondMethod;
+
+            pointer += delegate (int x, int y)
+            {
+                Console.WriteLine("I have two parameters " + x + "," + y + "]");
+            };
+
+            pointer += (int z, int l) =>
+            {
+                Console.WriteLine(z / l);
+            };
+
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            pointer(20, 5);
+
+            pointer -= newClass.MyFirstMethod;
+            pointer -= newClass.MySecondMethod;
+
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.Red;
+            pointer(20, 5);
+        }
+    }
+}
+public class MyClass
+{
+    public void MyFirstMethod(int a, int b)
+    {
+        Console.WriteLine(a * a * b);
+    }
+
+    public void MySecondMethod(int a, int b)
+    {
+        Console.WriteLine(a + a + a + b);
+    }
+}
